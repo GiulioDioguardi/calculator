dff0605ecda81950d33efa71d06c4f746766d3d3
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2015-10-23 12:57:23 +0200

    Written tests for addition, substraction, multiplication and division

---
+++ CalculatorUnitTest/CalculatorTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        Operations calculations = new Operations();
+        [TestMethod]
+        public void AddTest()
+        {
+            double result = calculations.NormalOperation("+", 2, 2);
+            Assert.AreEqual(4, result);
+            result = calculations.NormalOperation("+", -4, 2);
+            Assert.AreEqual(-2, result);
+        }
+
+        [TestMethod]
+        public void SubstractTest()
+        {
+            double result = calculations.NormalOperation("-", 4, 2);
+            Assert.AreEqual(2, result);
+            result = calculations.NormalOperation("-", 2, 4);
+            Assert.AreEqual(-2, result);
+        }
+
+        [TestMethod]
+        public void MultiplicationTest()
+        {
+            double result = calculations.NormalOperation("*", 4, 2);
+            Assert.AreEqual(8, result);
+            result = calculations.NormalOperation("*", -2, 4);
+            Assert.AreEqual(-8, result);
+            result = calculations.NormalOperation("*", 2.51, 4);
+            Assert.AreEqual(10.04, result);
+        }
+
+        [TestMethod]
+        public void DivisionTest()
+        {
+            double result = calculations.NormalOperation("/", 4, 2);
+            Assert.AreEqual(2, result);
+            result = calculations.NormalOperation("/", 4.6, 2);
+            Assert.AreEqual(2.3, result);
+            result = calculations.NormalOperation("/", 10, 0);
+            Assert.AreEqual(double.PositiveInfinity, result);
+            result = calculations.NormalOperation("/", -10, 0);
+            Assert.AreEqual(double.NegativeInfinity, result);
+        }
+    }
+}
